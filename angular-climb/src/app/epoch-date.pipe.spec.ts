5028163d93bbe185ea1c80df9207a6bcfe0a52f8
Magni <magni.thor@gmail.com>
2018-01-07 20:39:02 +0000

    epoch test

--- angular-climb/src/app/epoch-date.pipe.spec.ts
+++ angular-climb/src/app/epoch-date.pipe.spec.ts
@@ -9,8 +9,20 @@ describe('EpochDatePipe', () => {
     const pipe = new EpochDatePipe();
     expect(pipe.transform(null)).toBeNull();
   });
-  it ('check value', () => {
+  it ('check value 0', () => {
     const pipe = new EpochDatePipe();
-    expect(pipe.transform("/Date(0)/")).toBe(new Date(0));
+    expect(pipe.transform("/Date(0)/")).toEqual(new Date(0));
+  });
+  it ('check value -1000', () => {
+    const pipe = new EpochDatePipe();
+    expect(pipe.transform("/Date(-1000)/")).toEqual(new Date(-1000*1000));
+  });
+  it ('check value 1000', () => {
+    const pipe = new EpochDatePipe();
+    expect(pipe.transform("/Date(1000)/")).toEqual(new Date(1000*1000));
+  });
+  it ('check value 1000.5', () => {
+    const pipe = new EpochDatePipe();
+    expect(pipe.transform("/Date(1000.5)/")).toEqual(new Date(1000.5*1000));
   });
 });
