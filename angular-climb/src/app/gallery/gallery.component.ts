21c92cbe4a197f6ec0a95a39f489005ba4d6deef
Gummi St <gummi@climbing.is>
2018-03-01 21:11:11 +0000

    fixes

--- angular-climb/src/app/gallery/gallery.component.ts
+++ angular-climb/src/app/gallery/gallery.component.ts
@@ -2,11 +2,15 @@ import { ArticleDetails } from './../articleDetails';
 
 import { Component, OnInit, Input } from '@angular/core';
 
+//import { lightgallery } from './../../../node_modules/angular-lightgallery';
+
+
 @Component({
   selector: 'app-gallery',
   templateUrl: './gallery.component.html',
   styleUrls: ['./gallery.component.css']
 })
+
 export class GalleryComponent implements OnInit {
 
   @Input() art: ArticleDetails;
