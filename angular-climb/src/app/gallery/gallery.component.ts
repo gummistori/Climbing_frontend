fb1fb0d99273309b5454617fadb4853a2e8e31aa
Magni <magni.thor@gmail.com>
2018-03-02 00:01:15 +0000

    gallery

--- angular-climb/src/app/gallery/gallery.component.ts
+++ angular-climb/src/app/gallery/gallery.component.ts
@@ -1,9 +1,12 @@
 import { ArticleDetails } from './../articleDetails';
 
-import { Component, OnInit, Input } from '@angular/core';
+import { Component,  AfterViewInit , Input, HostListener } from '@angular/core';
 
-//import { lightgallery } from './../../../node_modules/angular-lightgallery';
+// import { lightgallery } from './../../../node_modules/angular-lightgallery';
 
+
+declare var lightGallery: any;
+declare var $: any;
 
 @Component({
   selector: 'app-gallery',
@@ -11,13 +14,51 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./gallery.component.css']
 })
 
-export class GalleryComponent implements OnInit {
+export class GalleryComponent implements  AfterViewInit  {
 
   @Input() art: ArticleDetails;
 
-  constructor() { }
+  Full = false;
+  FullIndex = 0;
 
-  ngOnInit() {
+  constructor() {
+  }
+
+  ngAfterViewInit() {
+    // debugger;
+   // $('#lightgallery').lightGallery();
+  }
+
+  @HostListener('window:keyup', ['$event'])
+  keyEvent(event: KeyboardEvent) {
+    switch (event.keyCode) {
+      case 39: this.next(); break;
+      case 37: this.prev(); break;
+      case 27: this.Full = false; break;
+    }
+  }
+
+  fullScreen(i) {
+    this.FullIndex = i;
+    this.Full = true;
+  }
+
+  fullExit() {
+    this.Full = false;
+  }
+  next() {
+    if (this.FullIndex === this.art.myndasida.length - 1) {
+      this.FullIndex = 0;
+    } else {
+      this.FullIndex++;
+    }
+  }
+  prev() {
+    if (this.FullIndex === 0) {
+      this.FullIndex = this.art.myndasida.length - 1;
+    } else {
+      this.FullIndex--;
+    }
   }
 
 }
