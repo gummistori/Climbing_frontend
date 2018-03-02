feaf673d1c581ff29ba92f8f66205015300cfdf5
Gummi St <gummi@climbing.is>
2018-03-02 01:33:18 +0000

    galleryfixes
    Not completed, fullscreen support version1

--- angular-climb/src/app/gallery/gallery.component.ts
+++ angular-climb/src/app/gallery/gallery.component.ts
@@ -1,9 +1,59 @@
+import { element } from 'protractor';
 import { ArticleDetails } from './../articleDetails';
 
-import { Component,  AfterViewInit , Input, HostListener } from '@angular/core';
+import { Component,  AfterViewInit , Input, HostListener, ElementRef } from '@angular/core';
 
 // import { lightgallery } from './../../../node_modules/angular-lightgallery';
 
+interface FsDocument extends HTMLDocument {
+  mozFullScreenElement?: Element;
+  msFullscreenElement?: Element;
+  msExitFullscreen?: () => void;
+  mozCancelFullScreen?: () => void;
+}
+
+export function isFullScreen(): boolean {
+  const fsDoc = <FsDocument> document;
+
+  return !!(fsDoc.fullscreenElement || fsDoc.mozFullScreenElement || fsDoc.webkitFullscreenElement || fsDoc.msFullscreenElement);
+}
+
+interface FsDocumentElement extends HTMLElement {
+  msRequestFullscreen?: () => void;
+  mozRequestFullScreen?: () => void;
+}
+
+export function toggleFullScreen(): void {
+  const fsDoc = <FsDocument> document;
+
+  if (!isFullScreen()) {
+    const fsDocElem = <FsDocumentElement> document.documentElement;
+
+    if (fsDocElem.requestFullscreen) {
+      fsDocElem.requestFullscreen();
+    } else if (fsDocElem.msRequestFullscreen) {
+      fsDocElem.msRequestFullscreen();
+    } else if (fsDocElem.mozRequestFullScreen) {
+      fsDocElem.mozRequestFullScreen();
+    } else if (fsDocElem.webkitRequestFullscreen) {
+      fsDocElem.webkitRequestFullscreen();
+    }
+  } else if (fsDoc.exitFullscreen) {
+    fsDoc.exitFullscreen();
+  } else if (fsDoc.msExitFullscreen) {
+    fsDoc.msExitFullscreen();
+  } else if (fsDoc.mozCancelFullScreen) {
+    fsDoc.mozCancelFullScreen();
+  } else if (fsDoc.webkitExitFullscreen) {
+    fsDoc.webkitExitFullscreen();
+  }
+}
+
+export function setFullScreen(full: boolean): void {
+  if (full !== isFullScreen()) {
+    toggleFullScreen();
+  }
+}
 
 declare var lightGallery: any;
 declare var $: any;
@@ -34,16 +84,19 @@ export class GalleryComponent implements  AfterViewInit  {
     switch (event.keyCode) {
       case 39: this.next(); break;
       case 37: this.prev(); break;
-      case 27: this.Full = false; break;
+      case 27: this.fullExit(); break;
     }
   }
 
   fullScreen(i) {
     this.FullIndex = i;
     this.Full = true;
+    setFullScreen(true);
+    // element. webkitRequestFullscreen()
   }
 
   fullExit() {
+    setFullScreen(false);
     this.Full = false;
   }
   next() {
