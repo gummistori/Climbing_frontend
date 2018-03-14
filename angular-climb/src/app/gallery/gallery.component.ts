2e9332b56fe20962e3ebd12f08e30c3d92e97f5e
Magni <magni.thor@gmail.com>
2018-03-14 20:52:49 +0000

    Ani

--- angular-climb/src/app/gallery/gallery.component.ts
+++ angular-climb/src/app/gallery/gallery.component.ts
@@ -70,8 +70,11 @@ export class GalleryComponent implements  AfterViewInit  {
 
   Full = false;
   FullIndex = 0;
+  FullIndexOld = 0;
   FullIndex1 = 0;
   FullIndexMinus1 = 0;
+  FullIndex2 = 0;
+  FullIndexMinus2 = 0;
 
   constructor() {
   }
@@ -92,6 +95,11 @@ export class GalleryComponent implements  AfterViewInit  {
 
   fullScreen(i) {
     this.FullIndex = i;
+    this.FullIndexOld = i;
+    this.FullIndex1 = this.nextValue(i);
+    this.FullIndexMinus1 = this.prevValue(i);
+    this.FullIndex2 = this.nextValue(this.FullIndex1);
+    this.FullIndexMinus2 = this.prevValue(this.FullIndexMinus1);
     this.Full = true;
     setFullScreen(true);
     document.body.style.overflow = 'hidden';
@@ -103,19 +111,46 @@ export class GalleryComponent implements  AfterViewInit  {
     this.Full = false;
     document.body.style.overflow = 'scroll';
   }
-  next() {
-    if (this.FullIndex === this.art.myndasida.length - 1) {
-      this.FullIndex = 0;
+  nextValue(value) {
+    if (value === this.art.myndasida.length - 1) {
+      return 0;
     } else {
-      this.FullIndex++;
+      return value + 1;
     }
   }
-  prev() {
-    if (this.FullIndex === 0) {
-      this.FullIndex = this.art.myndasida.length - 1;
+  prevValue(value) {
+    if (value === 0) {
+      return this.art.myndasida.length - 1;
     } else {
-      this.FullIndex--;
+      return value - 1;
     }
+  }
+  next() {
+    this.FullIndexOld = this.FullIndex;
+    setTimeout(() => { document.getElementById('newImage').className = 'image'; }, 500);
+    document.getElementById('newImage').className = 'image nextShow';
+
+    setTimeout(() => { document.getElementById('oldImage').className = 'hide'; }, 500);
+    document.getElementById('oldImage').className = 'image nextHide';
+
+    this.FullIndex = this.nextValue(this.FullIndex);
+    this.FullIndex1 = this.nextValue(this.FullIndex1);
+    this.FullIndexMinus1 = this.nextValue(this.FullIndexMinus1);
+    this.FullIndex2 = this.nextValue(this.FullIndex2);
+    this.FullIndexMinus2 = this.nextValue(this.FullIndexMinus2);
+  }
+  prev() {
+    this.FullIndexOld = this.FullIndex;
+    setTimeout(() => { document.getElementById('newImage').className = 'image'; }, 500);
+    document.getElementById('newImage').className = 'image prevShow';
+
+    setTimeout(() => { document.getElementById('oldImage').className = 'hide'; }, 500);
+    document.getElementById('oldImage').className = 'image prevHide';
+    this.FullIndex = this.prevValue(this.FullIndex);
+    this.FullIndex1 = this.prevValue(this.FullIndex1);
+    this.FullIndexMinus1 = this.prevValue(this.FullIndexMinus1);
+    this.FullIndex2 = this.prevValue(this.FullIndex2);
+    this.FullIndexMinus2 = this.prevValue(this.FullIndexMinus2);
   }
 
 }
