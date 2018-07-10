35c96a34a5a1d3900c6de5ee23803a4e48143801
Gummi St <gummi@climbing.is>
2018-07-10 21:17:06 +0000

    Bing maps instead of Google maps

--- angular-climb/src/app/article/article.component.ts
+++ angular-climb/src/app/article/article.component.ts
@@ -6,7 +6,7 @@ import { debug } from 'util';
 import { Tag } from './../data.service';
 
 declare var SWMap: any;
-declare var google: any;
+declare var Microsoft: any;
 
 @Component({
   selector: 'app-article',
@@ -26,8 +26,7 @@ export class ArticleComponent implements OnInit {
   lat: number = null;
   lon: number = null;
   textColor = 1;
-  cssClasses = {'text-white': true
-  , 'text-black': false};
+  cssClasses = {'text-white': true, 'text-black': false};
   tags: Tag[] = null;
 
   hasVideo = false;
@@ -64,8 +63,8 @@ export class ArticleComponent implements OnInit {
           }
           if (!document.getElementById('GoogleMapLibrary') && this.lat !== null && this.lon !== null) {
             const script = document.createElement('script');
-            script.src = 'https://maps.googleapis.com/maps/api/js';
-            script.id = 'SiteWatchLibrary';
+            script.src = 'https://www.bing.com/api/maps/mapcontrol?key=AvqjeaC_aQhAJre-FOfeoQcF-AHsJz5WaAJP2CYtDuAlO7z7Bx_sr_ZrpyZXieRK';
+            script.id = 'BingMaps';
             const me = this;
             script.onload = function() {  window.setTimeout(function(){ me.map(); }, 100); };
             document.body.appendChild( script );
@@ -130,44 +129,22 @@ export class ArticleComponent implements OnInit {
 
     if (this.lon !== null && this.lat !== null) {
       try {
-        if (!google) {
+        if (!Microsoft) {
           return;
         }
       } catch (Exception) {
         return;
       }
-      const mapOptions = {
-        zoom: 5,
-        disableDefaultUI: true,
-        mapTypeId: google.maps.MapTypeId.TERRAIN,
-        center: new google.maps.LatLng(this.lat, this.lon),
-        styles: [
-          {'featureType': 'all', 'elementType': 'all', 'stylers': [{'saturation': '20'}]},
-          {'featureType': 'administrative', 'elementType': 'labels', 'stylers': [{'visibility': 'on'}]},
-          {'featureType': 'administrative.province', 'elementType': 'all', 'stylers': [{'visibility': 'off'}]},
-          {'featureType': 'administrative.province', 'elementType': 'labels', 'stylers': [{'visibility': 'off'}]},
-          {'featureType': 'administrative.locality', 'elementType': 'all', 'stylers': [{'visibility': 'on'}]},
-          {'featureType': 'administrative.neighborhood', 'elementType': 'all', 'stylers': [{'visibility': 'off'}]},
-          {'featureType': 'administrative.land_parcel', 'elementType': 'all', 'stylers': [{'visibility': 'off'}]},
-          {'featureType': 'landscape', 'elementType': 'all', 'stylers': [{'saturation': '-47'}, {'lightness': '0'}, {'hue': '#00ffcd'}]},
-          {'featureType': 'landscape.man_made', 'elementType': 'all', 'stylers': [{'visibility': 'off'}]},
-          {'featureType': 'landscape.natural.terrain', 'elementType': 'all', 'stylers': [{'lightness': '0'}]},
-          {'featureType': 'poi', 'elementType': 'all', 'stylers': [{'lightness': '0'}, {'visibility': 'off'}, {'saturation': '-82'}]},
-          {'featureType': 'road', 'elementType': 'labels', 'stylers': [{'visibility': 'off'}]},
-          {'featureType': 'road.highway.controlled_access', 'elementType': 'all', 'stylers': [{'visibility': 'on'}]},
-          {'featureType': 'road.highway.controlled_access', 'elementType': 'labels', 'stylers': [{'visibility': 'off'}]},
-          {'featureType': 'transit', 'elementType': 'all', 'stylers': [{'visibility': 'off'}]},
-          {'featureType': 'water', 'elementType': 'all', 'stylers': [{'saturation': '-82'}, {'lightness': '-60'}, {'hue': '#009eff'}]},
-          {'featureType': 'water', 'elementType': 'labels', 'stylers': [{'visibility': 'off'}]}]};
-      const mapElement = document.getElementById('map');
-      const map = new google.maps.Map(mapElement, mapOptions);
-      const marker = new google.maps.Marker(
-      {
-        position: new google.maps.LatLng(this.lat, this.lon),
-        map: map,
-        icon: 'http://new.climbing.is/img/poi.png',
-        title: 'Climbing.is'
-      });
+      const map = new Microsoft.Maps.Map(document.getElementById('map'), {
+        center: new Microsoft.Maps.Location(this.lat, this.lon)
+        , mapTypeId: Microsoft.Maps.MapTypeId.aerial
+        , zoom: 5
+        });
+        /*
+        const pushpin = new Microsoft.Maps.Pushpin(map.getCenter(), { color: 'red' });
+        map.entities.push(pushpin);
+        // new google.maps.Map(mapElement, mapOptions);
+        */
     }
   }
 
